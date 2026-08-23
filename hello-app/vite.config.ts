2b2d9da4bec51d5e2273cca290e4b5f659c3ffda
Rae <101021941+xssrae@users.noreply.github.com>
2026-08-23 19:54:12 -0300

    feat: test files

--- hello-app/vite.config.ts
+++ hello-app/vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/hello-app',
   server: {
     port: 8081,
     strictPort: true,
     allowedHosts: true,
     host: true,
-  }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 })
